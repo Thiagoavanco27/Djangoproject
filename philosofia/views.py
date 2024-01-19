db01c015ff63354549d4a72b16cae6f64615462d
Thiago Reis <thiago.reis@faculdade.impacta.com.br>
2024-01-19 10:50:38 -0300

    CONFIG URL ID

--- philosofia/views.py
+++ philosofia/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'philosofia/pages/index.html' )
+    return render(request, 'philosofia/pages/index.html', context={
+        'name': 'Thiago Reis',
+    })
+
+def meditation(request, id):
+    return render(request, 'philosofia/pages/index.html', context={
+        'name': 'Thiago Reis',
+    })
