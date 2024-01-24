b3f42dd9ece711ac4bbf7562aa885ebbd8359ee8
Thiago Reis <thiago.reis@faculdade.impacta.com.br>
2024-01-24 17:59:04 -0300

    app_name

--- philosofia/urls.py
+++ philosofia/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "philosophies"
+
 urlpatterns = [
     path("", views.home, name="Home"),
     path("meditations/<int:id>/", views.meditation, name="Meditation"),
