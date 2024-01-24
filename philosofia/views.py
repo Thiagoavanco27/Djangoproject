de7126e301e4c7e70833d47760a99c104474849c
Thiago Reis <thiago.reis@faculdade.impacta.com.br>
2024-01-24 11:56:56 -0300

    faker e utils

--- philosofia/views.py
+++ philosofia/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
+from utils.philo.factory import MakeMeditation
+
 
 def home(request):
     return render(request, 'philosofia/pages/index.html', context={
-        'name': 'Thiago Reis',
+        'meditations': [MakeMeditation() for _ in range(10)],
     })
 
 def meditation(request, id):
     return render(request, 'philosofia/pages/meditation-view.html', context={
-        'name': 'Thiago Reis',
+        'meditation': MakeMeditation(),
     })
